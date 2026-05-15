4a888b6df969f73a50f12d09755facf2cd8b5d2f
mat7g <kashiginmatvej@inbox.ru>
2026-05-15 20:35:27 +0300

    Add files via upload

---
+++ all.bat
@@ -0,0 +1,17 @@
+echo off
+
+md Kashigin\Matvey\Igorevich
+pause
+
+echo > Kashigin\27042005.txt
+echo > Kashigin\Matvey\Igorevich\1.txt
+pause
+
+del Kashigin\27042005.txt
+del Kashigin\Matvey\Igorevich\1.txt
+pause
+
+rd Kashigin\Matvey\Igorevich
+rd Kashigin\Matvey
+rd Kashigin
+pause
